5955993714712b8de974184d611bba9078adc152
Borut Lužar <borut.luzar@gmail.com>
2026-03-19 09:51:32 +0100

    Add project files.

---
+++ RST_Prog3_Vaje/Program.cs
@@ -0,0 +1,39 @@
+﻿using RST2_Programiranje_Vaje_25_26;
+using static System.Collections.Specialized.BitVector32;
+
+namespace RST_Prog3_Vaje
+{
+    internal class Program
+    {
+        enum Tutorial
+        {
+            Tutorial_01 = 1,  // 16. 3. 2026
+            Tutorial_02 = 2,  // 20. 3. 2026
+            Tutorial_03 = 3,  // 25. 3. 2026
+            Tutorial_04 = 4,  //  7. 4. 2026
+            Tutorial_05 = 5,  //  9. 4. 2026
+            Tutorial_06 = 6,  // 10. 4. 2026
+            Tutorial_07 = 7,  // 13. 4. 2026
+            Tutorial_08 = 8,  // 23. 4. 2026
+            Tutorial_09 = 9,  //  6. 5. 2026
+            Tutorial_10 = 10, // 13. 5. 2026
+            Tutorial_11 = 11, // 15. 5. 2026
+            Tutorial_12 = 12, // 20. 5. 2026
+            Tutorial_13 = 13, // 21. 5. 2026
+            Tutorial_14 = 14, // 26. 5. 2026
+            Tutorial_15 = 15, // 28. 5. 2026
+        }
+
+        static void Main(string[] args)
+        {
+            switch (InterfaceFunctions.ChooseOption<Tutorial>())
+            {
+                case Tutorial.Tutorial_01:
+                    {
+                        Tutorials_01.Exercise_324();
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ RST_Prog3_Vaje/Tutorials_01.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace RST_Prog3_Vaje
+{
+    public class Tutorials_01
+    {
+        /// <summary>
+        /// Pripravite razred Index z ustreznimi lastnostmi, predvsem naj vsebuje seznam predmetov. 
+        /// Posamezen predmet naj bo instanca razreda Subject, ki ima lastnosti Name in Grade.
+        /// Lastnost Grade naj dovoli samo vrednosti med 5 in 10. 
+        /// Če poskušate nastaviti drugo vrednost, 
+        /// naj se samodejno popravi na najbližjo dovoljeno vrednost.
+        /// V razred Indeks dodajte še lastnost PovprecnaOcena kot samo za branje, 
+        /// ki izračuna povprečje ocen predmetov.
+        /// </summary>
+        public static void Exercise_324()
+        {
+            // Kreiramo nov indeks:
+            Index index = new Index(20, StudyProgram.RST);
+            index.Year = 2;
+
+            Subject prog3 = new Subject(1)
+            {
+                Name = "Programiranje 3",
+                Grade = 10,
+                ECTS = 6,
+                DateCompleted = new DateOnly(2026, 6, 4)
+            };
+            Subject baze = new Subject(2)
+            {
+                Name = "Baze in modeliranje podatkov",
+                Grade = 10,
+                ECTS = 6,
+                DateCompleted = new DateOnly(2026, 6, 10)
+            };
+
+            index.Subjects.Add(prog3);
+            index.Subjects.Add(baze);
+
+            Console.WriteLine($"{index}");
+
+            Console.WriteLine($"Povprečje ocen študenta {index.StudentID} je {index.AverageGrade:0.00}");
+            Console.Read();
+        }
+    }
+
+
+    /// <summary>
+    /// Razred, ki predstavlja študentski indeks.
+    /// </summary>
+    public class Index
+    {
+        public Index(int id, StudyProgram program)
+        {
+            this.StudentID = id;
+            this.Program = program;
+        }
+
+        public int StudentID { get; }
+
+        public StudyProgram Program { get; }
+
+        private int year;
+        
+        /// <summary>
+        /// Lastnost implementiramo v celoti, da lahko dodamo validacijo vrednosti ob nastavljanju
+        /// </summary>
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value < 1 || value > 3)
+                {
+                    throw new ArgumentException("Napačno leto študija!");
+                }
+                year = value;
+            }
+        }
+
+        public List<Subject> Subjects { get; } = new List<Subject>();
+
+        public double AverageGrade
+        {
+            get
+            {
+                return Subjects.Average(x => x.Grade);
+            }
+        }
+
+        public override string ToString()
+        {
+            string output = $"{this.StudentID} ({this.Program}): ";
+            foreach (var subject in this.Subjects)
+            {
+                output += $"\n\t{subject}";
+            }
+            return output;
+        }
+    }
+
+    /// <summary>
+    /// Enumeracija s študijskimi programi
+    /// </summary>
+    public enum StudyProgram
+    {
+        RST = 1,
+        ISD = 2,
+        RVRR = 3,
+        iRST = 4,
+        iISD = 5
+    }
+
+    /// <summary>
+    /// Razred, ki predstavlja posamezen predmet
+    /// </summary>
+    public class Subject
+    {
+        public Subject(int id)
+        {
+            this.ID = id;
+        }
+
+        public int ID { get; }
+        public string Name { get; set; }
+        public int ECTS { get; set; }
+
+        private int grade;
+        public int Grade 
+        {
+            get { return grade; }
+            set
+            {
+                if (value < 5)
+                {
+                    grade = 5;
+                }
+                else if(value > 10)
+                {
+                    grade = 10;
+                }
+                else
+                {
+                    grade = value;
+                }                
+            } 
+        }
+        public DateOnly DateCompleted { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Grade} [{ECTS}], opravljeno: {DateCompleted:dd.MM.yyyy}";
+        }
+    }
+}
